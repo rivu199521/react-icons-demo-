1e1be159f731802b8570b7cd8929329f36d149ee
unknown <mukherjeerivu7@gmail.com>
2020-07-27 16:22:13 +0530

    Updates

---
+++ precache-manifest.e469fc2ecbdb013a2aee63ffdd923256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0e04997422136e8d0960e5eda605da",
+    "url": "/react-icons-demo/index.html"
+  },
+  {
+    "revision": "54ee7be51a4adb404e09",
+    "url": "/react-icons-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "23825377611c9dfd2e69",
+    "url": "/react-icons-demo/static/js/2.590a6373.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-icons-demo/static/js/2.590a6373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ee7be51a4adb404e09",
+    "url": "/react-icons-demo/static/js/main.ae347469.chunk.js"
+  },
+  {
+    "revision": "074470a45b68d5aa456b",
+    "url": "/react-icons-demo/static/js/runtime-main.57f84876.js"
+  }
+]);
